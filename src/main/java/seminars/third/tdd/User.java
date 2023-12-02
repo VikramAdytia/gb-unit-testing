8ba9177c9716593f785c944179d1112f22aab679
VikramAdytia <Rundarreg@yandex.ru>
2023-12-02 18:23:02 +0300

    Урок 3. Качество тестов

--- src/main/java/seminars/third/tdd/User.java
+++ src/main/java/seminars/third/tdd/User.java
@@ -1,21 +1,28 @@
 package seminars.third.tdd;
 
 public class User {
-
     String name;
     String password;
 
-    boolean isAuthenticate = false;
+    boolean isAuthenticate;
+    boolean isAdmin;
 
     public User(String name, String password, boolean isAdmin) {
         this.name = name;
         this.password = password;
-
+        this.isAdmin = true;
     }
 
-    //3.6.
     public boolean authenticate(String name, String password) {
-        return false;
+        if (this.name.equals(name) && this.password.equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    @Override
+    public String toString() {
+        return "User: " + this.name + ", password: " + this.password + ".";
+    }
 }
--- src/main/java/seminars/third/tdd/UserRepository.java
+++ src/main/java/seminars/third/tdd/UserRepository.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepository {
+    List<User> data = new ArrayList<> ();
 
-    // Тут можно хранить аутентифицированных пользователей
-    List<User> data = new ArrayList<>();
+    public UserRepository() {
+        this.data = data;
+    }
 
-    public void addUser(User user) {
-       //..
+    public boolean addUser(User user) {
+        if (findByName(user.name)) {
+            data.add(user);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public boolean findByName(String username) {
@@ -17,6 +24,14 @@ public class UserRepository {
             if (user.name.equals(username)) {
                 return true;
             }
+        }
+        return false;
+    }
+
+    public boolean loginDeletedUser(User user) {
+        if (!user.isAuthenticate) {
+            data.remove(user);
+            return true;
         }
         return false;
     }
