7fb8eb9e4f44a4ac4ef429a95626c8dfc6f5ab70
VikramAdytia <Rundarreg@yandex.ru>
2023-12-11 16:51:33 +0300

    Промежуточная аттестация

---
+++ src/main/java/seminars/Final/model/AbstractClass.java
@@ -0,0 +1,11 @@
+package seminars.Final.model;
+
+public abstract class AbstractClass {
+    int[] array;
+
+    double resultAverage;
+
+    int[] createArray() {
+        return new int[10];
+    }
+}
